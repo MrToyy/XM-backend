13830f96e34aca76bd5693a4b72e2b89e6a18ba0
MrToyy <jason.wutong@gmail.com>
2013-06-19 23:03:11 +0800

    20130619-home

--- XMClass.js
+++ XMClass.js
@@ -2,11 +2,11 @@
 	
 	var XMEntry = Parse.Object.extend("XMEntry",{//instance methods
 		parseDescription: function(){
-			var des=new string(this.get("description").length);
+			var des=new string(this.get("description"));
 			if (des.length>0){
 				var desArr=des.split(" ");
 				if (desArr.length==1){
-					if isNaN(desArr[0]){
+					if (isNaN(desArr[0])){
 						this.set("returnCode","200");
 					}else{
 						var debitJournal = new Journal();
@@ -19,11 +19,53 @@
 						creditJournal.set("accountRef",searchRef("现金"));
 						this.add("journals",creditJournal);
 						
+						this.set("returnCode","100");
+						
 						debitJournal.destroy();
 						creditJournal.destroy();
 					}
 				}else{
-					for 
+					var f=false, debitStr="", creditStr="";
+					for (var i=0;i<desArr.length;i++){
+						if (isNaN(desArr[i]) || f ){
+							if (f){
+								debitStr=debitStr+desArr[i];
+							}else{
+								creditStr=creditStr+desArr[i];
+							}
+						}else{
+							f=true;
+							var am=desArr[i];
+						}
+					}
+					if (f){
+						var debitJournal = new Journal();
+						debitJournal.set("amount",am);
+						debitJournal.set("accountRef",searchRef(debitStr));
+						this.add("journals",debitJournal);
+						
+						var creditJournal = new Journal();
+						creditJournal.set("amount",-am);
+						creditJournal.set("accountRef",searchRef(creditStr));
+						this.add("journals",creditJournal);
+						
+						this.set("returnCode","100");
+						
+						debitJournal.destroy();
+						creditJournal.destroy();
+					}else{
+						this.set("returnCode","200");
+					}
+				}
+				switch (this.get("returnCode")){
+					case "100":
+						
+						break;
+					case "200":
+						
+						break;
+					default:
+						break;
 				}
 			}else{//description为空
 			
