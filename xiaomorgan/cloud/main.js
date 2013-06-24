5eb9f1f6f71fa0ef1b2891f23094a05dc3da7497
MrToyy <jason.wutong@gmail.com>
2013-06-24 23:22:09 +0800

    20130624-home

--- xiaomorgan/cloud/main.js
+++ xiaomorgan/cloud/main.js
@@ -10,11 +10,10 @@
 		if (l==1){//str长度为1，用equalTo进行查询
 			var q=new Parse.Query(Mapping);
 			q.equalTo("keyWords",str);
-			q.find({
-				success: function(results){
-					return results.get("accountRef");
-				},
-				error: function(error){
+			q.find().then(function(results){
+				if(results.length){
+					return results[0].get("accountRef");
+				}else{
 					return "99999";//未定义的暂存科目
 				}
 			});
@@ -24,11 +23,9 @@
 					var q=new Parse.Query(Mapping);
 					q.startsWith("keyWords",str.substr(j,i));
 					q.limit(1);
-					q.find({
-						success: function(results){
-							return results.get("accountRef");
-						},
-						error: function(error){
+					q.find().then(function(results){
+						if(results.length){
+							return results[0].get("accountRef");
 						}
 					});
 				}
@@ -43,15 +40,15 @@
 	var XMEntry = Parse.Object.extend("XMEntry",{
 		//instance methods
 		parseDescription: function(){//description为记账命令时调用
-			var Journal = Parse.Object.extend("Journals");//日记账类，作为XMEntry.journals的元素
+			//var Journal = Parse.Object.extend("Journals");//日记账类，作为XMEntry.journals的元素
 			var des=this.get("description")+"";
-			if (des.length>0){
-				var desArr=des.split(" ");
-				if (desArr.length==1){
-					if (isNaN(desArr)){
+			if (des.length>0){//des不为空
+				var desArr=des.split(" ");//用空格拆分
+				if (desArr.length==1){//des没有空格
+					if (Number(desArr[0])==NaN){//des没有空格且不为数字
 						this.set("returnCode","200");
-					}else{
-						this.set("amount",desArr);
+					}else{//des只有数字，为默认现金支出其他消费
+						this.set("amount",Number(desArr[0]));
 						this.set("debitRef",searchRef("其他"));
 						this.set("creditRef",searchRef("现金"));
 						
@@ -67,15 +64,15 @@
 						
 						this.set("returnCode","100");
 						
-						this.set("tmpStr",accountList.get(this.get("creditRef"))+"支付"+desArr+"元"+accountList.get(this.get("debitRef")));
+						//this.set("tmpStr",accountList.get(this.get("creditRef"))+"支付"+desArr+"元"+accountList.get(this.get("debitRef")));
 						
 						/*debitJournal.destroy();
 						creditJournal.destroy();*/
 					}
-				}else{
+				}else{//des拆分数量多于一个
 					var f=false, debitStr="", creditStr="";
-					for (var i=0;i<desArr.length;i++){
-						if (isNaN(desArr[i]) || f ){
+					for (var i=0;i<desArr.length;i++){//寻找第一个数字，该数字左边为贷方表述，右边为借方表述
+						if (Number(desArr[i])!=NaN || f ){
 							if (f){
 								debitStr=debitStr+desArr[i];
 							}else{
@@ -83,10 +80,10 @@
 							}
 						}else{
 							f=true;
-							var am=desArr[i];
+							var am=Number(desArr[i]);
 						}
 					}
-					if (f){
+					if (f){//找到了数字的情况，查找ref，没有则默认为现金支出其他消费
 						if (debitStr=="") debitStr="其他";
 						if (creditStr=="") creditStr="现金";
 						this.set("amount",am);
@@ -105,7 +102,7 @@
 						
 						this.set("returnCode","100");
 						
-						this.set("tmpStr",accountList.get(this.get("creditRef"))+"支付"+am+"元"+accountList.get(this.get("debitRef")));
+						//this.set("tmpStr",accountList.get(this.get("creditRef"))+"支付"+am+"元"+accountList.get(this.get("debitRef")));
 						
 						/*debitJournal.destroy();
 						creditJournal.destroy();*/
@@ -143,14 +140,7 @@
 			}
 		}
 	},{//class methods
-		newEntry: function(des, source){
-			var userEntry=new XMEntry();
-			//userEntry.set("user", user);
-			userEntry.set("source", source);
-			userEntry.set("description", des);
-			
-			return userEntry;
-		}
+	
 	});
 	
     
@@ -165,65 +155,38 @@
 	});
 	
 	
-	//处理用户输入，输出回复
-	function nnewEntry(des, source){
-		var userEntry=new XMEntry();
-		//userEntry.set("user", user);
-		userEntry.set("source", source);
-		userEntry.set("description", des);
-		
-		/*f (des.substr(0,1)=="?" || des.substr(0,1)=="？"){//用户指令
-			
-		}else{//用户日记账
-			userEntry.parseDescription();
-			userEntry.generateReply();
-			
-			//var userReport= XMReport.recordEntry(userEntry);
-			//userReport.save();
-			
-		}*/
-		userEntry.generateReply();
-		userEntry.save().then(function(userEntry){
-			return userEntry.get("reply");
-		},function(error){
-			
-		});
-	}
-	
-	
 	//-----------------------------------------------------------------------------
 	//微信接口
 Parse.Cloud.define("weixinInterface", function(request, response){
 	var FUser=Parse.Object.extend("FUser");//创建一个虚拟用户类
 	var qUser=new Parse.Query("FUser");
 	
-	var Reply=Parse.Object.extend("Reply");
-	var reply=new Reply();
-	
 	qUser.equalTo("userName",request.params.user);
-	qUser.find({
-		success:function(fUser){//已知用户
-			reply.set("msgType","text");
-			reply.set("text","Existing user");
-			reply.save().then(function(reply){
-				response.success(reply);
-			});
-		},
-		error:function(error){//新用户
-			var fUser=new FUser();
-			fUser.set("userName",request.params.user);
-			fUser.set("creatTime",Date()):
-			fUser.save().then(function(fUser){
-				reply.set("msgType","text");
-				reply.set("text","Welcome new user");
-				reply.save().then(function(reply){
-					response.success(reply);
-				});
-			},function(error){
-				response.error(error);
-			});
+	qUser.find().then(function(qUser){//判断是否为新用户
+		if (qUser.length){//已知用户
+			return qUser[0].fetch();
+		}else{//新用户*/
+			var nUser=new FUser();
+			nUser.set("userName",request.params.user);
+			nUser.set("creatTime",Date());
+			return nUser.save();
 		}
+	}).then(function(fUser){//取得并记录用户提交的内容
+		var userEntry=new XMEntry();
+		userEntry.set("user", fUser);
+		userEntry.set("source", request.params.source);
+		userEntry.set("description", request.params.content);
+		
+		userEntry.parseDescription();
+		userEntry.generateReply();
+		return userEntry.save()
+	}).then(function(userEntry){//回复用户
+		response.success(userEntry.get("reply"));
+	},function(error){
+		response.error(error);
 	});
+
+	
 	/*var userEntry=XMEntry.newEntry(request.params.content, request.params.source);
 	userEntry.generateReply();
 	userEntry.save().then(function(userEntry){
